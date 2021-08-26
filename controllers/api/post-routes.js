0361f8795cb1e3c89cca52418d5d10cb250cc852
Cory Piette <coryjpiette@gmail.com>
2021-08-26 16:27:21 -0400

    adding data posts to post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -61,3 +61,31 @@ router.get('/:id', (req, res) => {
         }
         ]
     })
+
+
+
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
